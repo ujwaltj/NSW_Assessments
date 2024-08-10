41456210f6d553b03c8daa199d888f2d5341bf0c
User <User@DESKTOP-R9F6DFD>
2024-08-10 06:52:34 +0530

    Changes to baseclass and updated the pom with maven-surefire-plugin

--- src/main/java/com/nsw/gov/framework/BaseClass.java
+++ src/main/java/com/nsw/gov/framework/BaseClass.java
@@ -9,7 +9,9 @@ import org.apache.commons.io.FileUtils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+
 
 
 public class BaseClass 
